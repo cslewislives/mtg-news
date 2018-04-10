6479291e5a41b7ae31caaa0807ee59915813036f
Joshua C.S. Lewis <cslewislives@gmail.com>
2018-04-10 17:00:00 -0400

    adds scrape and find all functionality

--- app/public/app.js
+++ app/public/app.js
@@ -2,9 +2,9 @@ $(document).ready(() => {
     $.getJSON('/articles', data => {
         for (let i in data) {
             console.log(data[i]);
-            $('#articles').append(`<p data-id='${data[i]._id}'>${data[i].title}
-            ${data[i].author}
-            ${data[i].description}
+            $('#articles').append(`<p data-id='${data[i]._id}'>${data[i].title}<br/>
+            ${data[i].author}<br/>
+            ${data[i].description}<br/>
             ${data[i].link}</p>`)
         }
     });
